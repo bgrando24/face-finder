dcc0ee3981118279870366ab122c3fa24aeffa4e
bgrando24 <bgrando24@gmail.com>
2025-08-28 14:32:30 +1000

    added FaceDetector class (WIP), plus image meta data endpoint

---
+++ src/face_detector.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy
+import matplotlib.pyplot as plt
+
+class FaceDetector:
+    
+    def __init__(self) -> None:
+        self.cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+    
+    def get_img_meta(self, image: numpy.ndarray):
+        """
+        Return metadata for the image image
+        
+        Args:
+            **image** - Image file as a numpy ndarray 
+        """
+        cv2_img = cv2.imdecode(image, cv2.IMREAD_COLOR)
+        
+        if image is None or cv2_img is None:
+            return None
+        
+        im_shape = cv2_img.shape
+        
+        return {
+            "height": im_shape[0],
+            "width": im_shape[1] if len(im_shape) > 1 else 1,
+            "channels": im_shape[2] if len(im_shape) > 2 else 1,
+            "compressed_size_bytes": len(image),
+            "uncompressed_size_bytes": cv2_img.nbytes,
+            "aspect_ratio": (im_shape[1] / im_shape[0]) if len(im_shape) > 1 and im_shape[0] != 0 else None,
+        }
+        
--- src/main.py
+++ src/main.py
@@ -1,9 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from face_detector import FaceDetector
+import numpy
 
 # init app
 app = FastAPI()
 
-# basic test
+#init face detector
+FD = FaceDetector()
+
+# index
 @app.get("/")
 async def get_root():
-    return {"message": "Hello world!"}
+    return {"message": "Welcome to Face Detector!"}
+
+# get image metadata
+@app.post("/meta-data")
+async def post_extract_img_meta(img_file: UploadFile = File(...)):
+    # read request contents, convert to numpy array
+    contents = await img_file.read()
+    img_np_array = numpy.frombuffer(contents, numpy.uint8)
+    # pass to face detector class
+    return FD.get_img_meta(img_np_array)
